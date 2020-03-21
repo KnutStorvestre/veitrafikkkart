66352471a62abc4ac07edfc5e09fb1a5e121fbd5
Knut Storvestre <huq006@uib.no>
2020-03-21 12:49:48 +0100

    firebase functions connection with firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,22 @@
-//import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
+export const getUserDecima = functions.https.onRequest((req, res) => {
+ const promise = admin.firestore().doc('users/Decima').get()
+ const p2 = promise.then(snapshot => {
+  const data = snapshot.data();
+  res.send(data)
+     });
+ p2.catch(error => {
+  console.log(error)
+  res.status(500).send(error)
+ })
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+ export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
