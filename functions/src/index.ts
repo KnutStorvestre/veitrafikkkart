1f1d9c122a0127bdc33578027d2f12f3f83225b2
Knut Storvestre <huq006@uib.no>
2020-03-22 13:43:13 +0100

    now able to get all documents in collection users in firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,31 @@ import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
+
+export const getUsers = functions.https.onRequest((req, res) => {
+ admin.firestore().collection('users').get()
+     .then((data => {
+      let scream = []
+      scream.push("")
+      data.forEach((doc) => {
+       scream.push(doc.data())
+      })
+      return res.json(scream)
+     }))
+     .catch((err) => console.log(err))
+})
+
 export const getUserDecima = functions.https.onRequest((req, res) => {
- const promise = admin.firestore().doc('users/Decima').get()
- const p2 = promise.then(snapshot => {
+ admin.firestore().doc('users/Decima').get()
+ .then(snapshot => {
   const data = snapshot.data();
   res.send(data)
-     });
- p2.catch(error => {
+     })
+ .catch(error => {
   console.log(error)
   res.status(500).send(error)
  })
-});
+})
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+//import * as serviceWorker from './serviceWorker';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDop8SvuITaKeWvxZK7fOiOLXWEPVU2uik",
+    authDomain: "veitrafikk-kart4.firebaseapp.com",
+    databaseURL: "https://veitrafikk-kart4.firebaseio.com",
+    projectId: "veitrafikk-kart4",
+    storageBucket: "veitrafikk-kart4.appspot.com",
+    messagingSenderId: "549360792341",
+    appId: "1:549360792341:web:8766270393aaa9428a2f1c",
+    measurementId: "G-2M8QC34TE2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
