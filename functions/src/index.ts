a5010aeac8965b2106169f23b958188d4becb4f7
Knut Storvestre <huq006@uib.no>
2020-03-22 14:51:20 +0100

    now able to add new user with function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,19 +2,46 @@ import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
+exports.createNewUser = functions.https.onRequest((req, res:any | undefined) => {
+
+    if (req.method !== 'POST')
+        return res.status(400).json({error: 'MMM'});
+    /*
+    if (req.method !== 'POST'){
+        return res.status(400).json({ error: 'Method not allowed'});
+    }
+     */
+
+    const newUser = {
+        first: req.body.body,
+        time: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin
+        .firestore()
+        .collection('users')
+        .add(newUser)
+        .then(doc => {
+            res.json({ message: `document ${doc.id} created successfully`});
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong'});
+            console.error(err)
+        })
+});
 
 export const getUsers = functions.https.onRequest((req, res) => {
  admin.firestore().collection('users').get()
      .then((data => {
-      let scream = []
-      scream.push("")
+      let scream = [];
+      scream.push("");
       data.forEach((doc) => {
        scream.push(doc.data())
-      })
+      });
       return res.json(scream)
      }))
      .catch((err) => console.log(err))
-})
+});
 
 export const getUserDecima = functions.https.onRequest((req, res) => {
  admin.firestore().doc('users/Decima').get()
@@ -23,10 +50,10 @@ export const getUserDecima = functions.https.onRequest((req, res) => {
   res.send(data)
      })
  .catch(error => {
-  console.log(error)
+  console.log(error);
   res.status(500).send(error)
  })
-})
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
