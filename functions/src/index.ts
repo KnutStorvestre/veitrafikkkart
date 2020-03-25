8f1f654f0b9c556df090c7dee22fa0c537aacb4f
Knut Storvestre <huq006@uib.no>
2020-03-25 17:03:31 +0100

    authentication is in mvp

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,23 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
-
+const app = require('express')();
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+    apiKey: "AIzaSyDop8SvuITaKeWvxZK7fOiOLXWEPVU2uik",
+    authDomain: "veitrafikk-kart4.firebaseapp.com",
+    databaseURL: "https://veitrafikk-kart4.firebaseio.com",
+    projectId: "veitrafikk-kart4",
+    storageBucket: "veitrafikk-kart4.appspot.com",
+    messagingSenderId: "549360792341",
+    appId: "1:549360792341:web:8766270393aaa9428a2f1c",
+    measurementId: "G-2M8QC34TE2"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+
 
 app.get('/screams',(req:undefined, res:undefined | any) => {
     admin
@@ -25,11 +38,11 @@ app.get('/screams',(req:undefined, res:undefined | any) => {
             return res.json(screams);
         })
         .catch((err) => console.log(err));
-})
+});
 
 app.post('/scream',(req:any, res:any | undefined) => {
     const newScream = {
-        first: req.body.body,
+        body: req.body.body,
         userHandle: req.body.userHandle,
         //turns date into string
         createdAt: new Date().toISOString()
@@ -69,6 +82,29 @@ exports.createNewUser = functions.https.onRequest((req, res:any | undefined) => 
             res.status(500).json({ error: 'something went wrong'});
             console.error(err)
         })
+});
+
+app.post('/signup',(req:any,res:any) => {
+    const newUser = {
+            email: req.body.email,
+            password: req.body.password,
+            confirmPassword: req.body.confirmPassword,
+            handle: req.body.handle
+    };
+
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(( data: any | undefined) => {
+            return res
+                .status(201)
+                .json({ message: `user ${data.user.uid} signed up successfully` })
+        })
+        .catch((err:any) => {
+            console.log("something went bad")
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        });
 });
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
