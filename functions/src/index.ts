ddcbdc837a650e68e28040420056070146b23e90
Knut Storvestre <huq006@uib.no>
2020-03-25 12:45:47 +0100

    functions can now get and create new screams

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,39 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin";
+
 admin.initializeApp();
 
+const express = require('express');
+const app = express();
 
+app.get('/screams',(req:undefined, res:undefined | any) => {
+    admin
+        .firestore()
+        .collection('screams')
+        //.orderBy('createdAt','desc') denne er buggy
+        .get()
+        .then((data) => {
+            let screams:any = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                })
+            });
+            return res.json(screams);
+        })
+        .catch((err) => console.log(err));
+})
 
-exports.createScream = functions.https.onRequest((req, res:any | undefined) => {
-
-    if (req.method != 'POST'){
-        return res.status(400).json({ error: 'Method not allowed'});
-    }
+app.post('/scream',(req:any, res:any | undefined) => {
     const newScream = {
         first: req.body.body,
         userHandle: req.body.userHandle,
-        time: admin.firestore.Timestamp.fromDate(new Date())
+        //turns date into string
+        createdAt: new Date().toISOString()
+        //time: admin.firestore.Timestamp.fromDate(new Date())
     };
 
     admin
@@ -50,34 +71,4 @@ exports.createNewUser = functions.https.onRequest((req, res:any | undefined) => 
         })
 });
 
-export const getUsers = functions.https.onRequest((req, res) => {
- admin.firestore().collection('users').get()
-     .then((data => {
-      let scream = [];
-      scream.push("");
-      data.forEach((doc) => {
-       scream.push(doc.data())
-      });
-      return res.json(scream)
-     }))
-     .catch((err) => console.log(err))
-});
-
-export const getUserDecima = functions.https.onRequest((req, res) => {
- admin.firestore().doc('users/Decima').get()
- .then(snapshot => {
-  const data = snapshot.data();
-  res.send(data)
-     })
- .catch(error => {
-  console.log(error);
-  res.status(500).send(error)
- })
-});
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+exports.api = functions.region('europe-west1').https.onRequest(app);
