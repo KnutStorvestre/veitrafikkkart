e0abcd458a6c5efdf23fd6eef5ace1aa30441244
Knut Storvestre <huq006@uib.no>
2020-04-09 11:04:09 +0200

    Created some basic pages in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <input type="file"/>
+        <h1>Our app</h1>
     </div>
   );
 }
---
+++ src/pages/signUp.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class signUp extends Component<any, any>{
+
+    render() {
+        return(
+            <div>
+                <h1>Sign up page</h1>
+            </div>
+        )
+    }
+}
+
+export default signUp;
