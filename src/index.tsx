d56e09a01c9f9f8e4350956a8924c652bacb8263
Knut Storvestre <huq006@uib.no>
2020-05-08 13:58:48 +0200

    Removed all api keys and added them to gitignore. There are hopefully no api keys that will be pushed to git.

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,7 @@ import App from './App';
 //import firebase from 'firebase'
 //import * as serviceWorker from './serviceWorker';
 /*
-var firebaseConfig = {
-    apiKey: "AIzaSyDop8SvuITaKeWvxZK7fOiOLXWEPVU2uik",
-    authDomain: "veitrafikk-kart4.firebaseapp.com",
-    databaseURL: "https://veitrafikk-kart4.firebaseio.com",
-    projectId: "veitrafikk-kart4",
-    storageBucket: "veitrafikk-kart4.appspot.com",
-    messagingSenderId: "549360792341",
-    appId: "1:549360792341:web:8766270393aaa9428a2f1c",
-    measurementId: "G-2M8QC34TE2"
-};
+
  */
 // Initialize Firebase
 //firebase.initializeApp(firebaseConfig);
